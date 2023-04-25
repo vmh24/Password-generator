b7beffc721b839e66bf32017a5305bd70dfb256c
vmh24 <125014499+vmh24@users.noreply.github.com>
2023-04-25 13:31:13 +0200

    1.0.0

---
+++ pwGen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*+,-./:;?@[\]^_{|}"
+length=${#chars}
+
+while true; do
+    echo "Enter a number: "
+    read n
+
+    if echo "$n" | grep -q "^[0-9]*$"; then
+        break
+    fi
+    echo "Invalid character" 
+done
+
+for (( i=1; i<=$n; i++ ))
+do
+    index=$((RANDOM % $length))
+    random_char=${chars:index:1}
+
+    password+=$random_char
+done
+
+echo $password
